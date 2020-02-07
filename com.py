6995237fc821d3980736f05c13ad0aacd71ad3d4
Chris Nelson <32886763+Chris300127@users.noreply.github.com>
2020-02-07 15:53:12 -0800

    update files via upload
    integration of can tracking

--- com.py
+++ com.py
@@ -1,57 +1,152 @@
+#For Communication
 import serial
 import time
 import random
 import threading
 
+#for Can Tracking
+import cv2
+import numpy as np
+import sys
 
-#GLOBAL VARIABLES
-max_x, max_y = 780,780 #max x and y coordinates of the Gantry system
-ENC = 'utf-8'   #encoding to be used over serial
+#For Leaf Tracking
+from matplotlib import pyplot as plt
 
+DROP_MUTEX = threading.Lock()
+should_drop = 0
 
-
-#setup serial port
-port = serial.Serial(timeout=None, port="/dev/ttyTHS1", baudrate=115200)   
-# s = serial.Serial(timeout=1, port="/dev/ttyTHS2", baudrate=115200)
-
-#clears any data that may be sitting in serial buffer on launch
-if port.in_waiting:
-    port.read(port.in_waiting)
+LEAF_MUTEX = threading.Lock()
 
 
-#this function will eventually return the coordinates of a leaf
-#for right now it is a placeholder that returns random coordinates
+ser = serial.Serial(timeout=None, port="/dev/ttyTHS1", baudrate=115200)
+
+def leafTrack():
+    global cx, cy
+    cap = cv2.VideoCapture(0)
+    while(True):
+        ret, frame = cap.read()
+        original = frame.copy()
+
+        sample = np.array(frame)
+        gray = cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray,(5,5),cv2.BORDER_DEFAULT)
+        canny = cv2.Canny(blurred,50,100)
+        kernel = np.ones((3,3),np.uint8)
+        dilate = cv2.dilate(canny, kernel, iterations=1)
+        cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts)==2 else cnts[1]
+        for c in cnts:
+            print('wow a leaf')
+            x,y,w,h = cv2.boundingRect(c)
+            cv2.rectangle(sample, (x,y), (x+w, y+h), (36,255,12), 2)
+            ROI = original[y:y+h, x:x+w]
+            r = 5
+            t = 1
+            # Center coordinates (cx, cy)
+            cx = int(w / 2)
+            cy = int(h / 2)
+            #
+            #cv2.circle(ROI, (cx, cy), r, (255, 0, 0), thickness=t)
+            #cv2.imshow("identifiedLeaf", ROI)
+        if cv2.waitKey(1)==27:  # esc key
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+def canTrack():
+    global should_drop
+    cap = cv2.VideoCapture(0)
+    count = 500
+    max_x = 500
+    max_y = 500
+    max_r = 500
+    while(True):
+        ret, frame = cap.read()
+        output = frame.copy()
+        gray = cv2.medianBlur(cv2.cvtColor(output, cv2.COLOR_BGR2GRAY),5)    #Take in video
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.4, 50)  #Currently only csv.HOUGH_GRADIENT for circle
+        #ensure at least some circles were found
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            count += 1
+            for (x,y,r) in circles: #FInd the circules
+                #Need to set the x threshold (ORIGINAL: )
+                #Offset will be # (need to be added to the last outputs)
+                if x > 50 and x < max_x and y > 150 and y < 300: #only want circles in certain areas
+                    max_x = x
+                    max_y = y
+                    max_r = r
+            #only output if there is circles and we scanned multiple times
+            if max_x != 500 and count > 2:
+                DROP_MUTEX.acquire()
+                if max_x < 150:
+                    # print("DROP " + str(max_x) + ", "+ str(max_y))
+                    should_drop = 1         ###MUTE
+                else:
+                    should_drop = 0
+                DROP_MUTEX.release()
+                cv2.circle(gray, (max_x,max_y), max_r, (0,255,0),4)
+                cv2.rectangle(gray, (max_x-5, max_y-5), (max_x+5, max_y+5), (0, 128, 255), -1)
+                max_x = 500
+                max_y = 500
+                max_r = 500
+                count = 500
+
+        cv2.imshow('video',gray)   #if we want to see the output
+        if cv2.waitKey(1)==27:# esc Key
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
 def calcCoordinates():
-    return (random.randint(0,max_x),random.randint(0,max_y))
+    return (random.randint(0,780),random.randint(0,780))
 
-#this function will eventually calculate time until drop and send drop signal
-#for now it waits a few seconds and then send a drop command
 def waitForCan():
-    time.sleep(random.randint(1,4))
-    port.write(bytes("go\n",ENC))
-
+    global should_drop
+    while True:
+        DROP_MUTEX.acquire()
+        if should_drop==1:
+            should_drop = 0
+            DROP_MUTEX.release()
+            return
+        DROP_MUTEX.release()
 
 def main():
-    print("Serial port open - listening for input") 
-    print("Press Ctrl+C to stop program")
-    in_message, out_message = None, None    #strings that will hold input and output from serial port
+    print("Listening")
+    ENC = 'utf-8'   #serial byte encoding spec
+    in_message = None
+    out_message = None
     try:
         while True:
-            if port.in_waiting>0:
-                in_message = (port.readline())[:-1] #read in message from serial port and drop the \n
-                
+            if ser.in_waiting>0:
+                in_message = (ser.readline())[:-1]
                 if in_message == b'coords?':
-                    #send coordinates of next leaf
                     out_message = f'{calcCoordinates()}\n'
-                    port.write(bytes(out_message,ENC))
+                    ser.write(bytes(out_message,ENC))
                 elif in_message == b'time?':
-                    #respond only when leaf should be dropped
                     waitForCan()
+                    ser.write(bytes("drop\n",ENC))
+                    print("now")
                 else:
-                    port.write(bytes("unknown command\n",ENC))
-                    print("Received unknown command")
+                    ser.write(bytes("unknown command\n",ENC))
     except KeyboardInterrupt:
-        print('\nkeyboard interupt -- serial port closed')
+        print('\nkeyboard interupt -- stopping server')
 
+
+
+
+
+
+can_tracking = threading.Thread(target=canTrack)
+can_tracking.daemon = True
+can_tracking.start()
+
+
+# s = serial.Serial(timeout=1, port="/dev/ttyTHS2", baudrate=115200)
+
+if ser.in_waiting:
+    ser.read(ser.in_waiting)
 
 main()
---
+++ read_camera_input_test.py
@@ -0,0 +1,83 @@
+### ------------------------
+### Summary: Use the RealSense Camera to identify center of leaves
+### Input: RealSense Depth Camera
+### Output: XY of the center & image of the leaf
+### Author: Sherene (sevictor@)
+### Notes: Needs white background 
+### ------------------------
+
+import numpy as np
+import cv2
+#import pyrealsense2 as rs
+from matplotlib import pyplot as plt
+
+# If VideoCapture and frame.copy fail, try unplugging and replugging the camera
+cap = cv2.VideoCapture(0)
+print("Press ESC to end program")
+
+while(True):
+    ret, frame = cap.read()
+    #print("ret: ", ret)
+    #print("frame: ", frame)
+    original = frame.copy()
+    output = frame.copy()
+
+    # GRABCUT METHOD - didn't work, not fast enough for video
+    #mask = np.zeros(output.shape[:2], np.uint8)
+    #bgdModel = np.zeros((1, 65), np.float64)
+    #fgdModel = np.zeros((1, 65), np.float64)
+    # Modify rect based on the size of your cropped/extracted image
+    #rect = (50, 50, 450, 290)
+    # rect is only used when mode==GC_INIT_WITH_RECT
+    #cv2.grabCut(output, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+    #mask2 = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
+    #croppedimg = frame*mask2[:,:,np.newaxis]
+
+    # SIFT METHOD - circuitdigest.com/tutorial 
+    # "Real Life Object Detection Using OpenCV Python Detecting Objects in Live Video
+    # STOPPED because working on development from Jupyter notebook demo
+    #height, width = frame.shape[:2]
+    #top_left_x = int(width / 3) 
+    #top_left_y = int((height / 2) + (height / 4))
+    #bottom_right_x = int((width / 3) * 2)
+    #bottom_right_y = int((height / 2) - (height / 4))
+    #print("height: ", height)
+    #print("width: ", width)
+    #cv2.rectangle(frame, )
+
+    # MY METHOD - based on Jupyter notebook demo (see team documentation)
+    #
+    sample = np.array(frame)
+    gray = cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray,(5,5),cv2.BORDER_DEFAULT)
+    canny = cv2.Canny(blurred,50,100)
+    kernel = np.ones((3,3),np.uint8)
+    dilate = cv2.dilate(canny, kernel, iterations=1)
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts)==2 else cnts[1]
+    for c in cnts:
+        print("wow a leaf")
+        x,y,w,h = cv2.boundingRect(c)
+        cv2.rectangle(sample, (x,y), (x+w, y+h), (36,255,12), 2)
+        # ROI = original[y:y+h, x:x+w]
+        r = 5
+        t = 1
+        cx = int(w / 2)
+        cy = int(h / 2)
+        print(cx,cy)
+        cv2.circle(sample, (x+cx, y+cy), r, (255, 0, 0), thickness=t)
+        cv2.imshow("identifiedLeaf", sample)
+
+    #cv2.imshow("croppedimg", croppedimg)
+    #cv2.imwrite("croppedimg.png", croppedimg)
+
+    #cv2.imshow('video', output)
+    #cv2.imwrite("screen.png", output)
+    #break
+    if cv2.waitKey(1)==27:  # esc key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
